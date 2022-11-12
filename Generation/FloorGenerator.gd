6bf42ba1b31c09e89b4e5f6a9badb7c00e40acfa
Dean Bolte <dean.bolte7@gmail.com>
2022-11-13 10:16:37 +1100

    created functions for creating rooms and spawning the player

--- Generation/FloorGenerator.gd
+++ Generation/FloorGenerator.gd
@@ -1,13 +1,29 @@
 extends Node2D
 
+export var ROOM_DISTANCE = 640
+
 onready var RoomsActive = $RoomsActive
 
 var BaseRoomScene = preload("res://Generation/RoomBase.tscn")
 var PlayerScene = preload("res://Player/Player.tscn")
 
+var RoomMap
+
 func _ready():
+	create_room(0, 0)
+	spawn_player(0, 0)
+	
+	
+	
+
+func spawn_player(x, y):
+	var playerInst = PlayerScene.instance()
+	add_child(playerInst)
+	
+	playerInst.global_position = Vector2(x, y)
+
+func create_room(x, y):
 	var roomInst = BaseRoomScene.instance()
 	RoomsActive.add_child(roomInst)
 	
-	var playerInst = PlayerScene.instance()
-	add_child(playerInst)
+	roomInst.global_position = Vector2(x, y)
