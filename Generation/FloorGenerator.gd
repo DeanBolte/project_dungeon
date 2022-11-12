61658475b5e0bca77c4a0a800156950c4bbd6298
Dean Bolte <dean.bolte7@gmail.com>
2022-11-13 10:09:45 +1100

    implemented generating starting room

--- Generation/FloorGenerator.gd
+++ Generation/FloorGenerator.gd
@@ -1,2 +1,13 @@
 extends Node2D
 
+onready var RoomsActive = $RoomsActive
+
+var BaseRoomScene = preload("res://Generation/RoomBase.tscn")
+var PlayerScene = preload("res://Player/Player.tscn")
+
+func _ready():
+	var roomInst = BaseRoomScene.instance()
+	RoomsActive.add_child(roomInst)
+	
+	var playerInst = PlayerScene.instance()
+	add_child(playerInst)
