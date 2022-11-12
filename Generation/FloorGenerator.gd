fa01ff30ef1a2f1382a29b1dd7bd67ab8f9224ee
Dean Bolte <dean.bolte7@gmail.com>
2022-11-13 10:52:54 +1100

    added basic map generation

--- Generation/FloorGenerator.gd
+++ Generation/FloorGenerator.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+
+
 export var ROOM_DISTANCE = 640
 
 onready var RoomsActive = $RoomsActive
@@ -7,23 +9,38 @@ onready var RoomsActive = $RoomsActive
 var BaseRoomScene = preload("res://Generation/RoomBase.tscn")
 var PlayerScene = preload("res://Player/Player.tscn")
 
-var RoomMap
+var roomMap = Dictionary()
+var Player
 
 func _ready():
-	create_room(0, 0)
-	spawn_player(0, 0)
-	
-	
-	
+	var startingRoom = create_room(0, 0)
+	Player = spawn_player(320, 320)
 
 func spawn_player(x, y):
 	var playerInst = PlayerScene.instance()
 	add_child(playerInst)
 	
 	playerInst.global_position = Vector2(x, y)
+	
+	return playerInst
 
 func create_room(x, y):
-	var roomInst = BaseRoomScene.instance()
-	RoomsActive.add_child(roomInst)
-	
-	roomInst.global_position = Vector2(x, y)
+	var keyString = "({x}, {y})"
+	var key = keyString.format({"x": x, "y": y})
+	if(not roomMap.has(key)):
+		var roomInst = BaseRoomScene.instance()
+		RoomsActive.add_child(roomInst)
+		
+		roomInst.global_position = Vector2(x*ROOM_DISTANCE, y*ROOM_DISTANCE)
+		roomInst.MAP_LOCATION = Vector2(x, y)
+		
+		roomInst.connect("first_entered", self, "generate_next")
+		roomMap[key] = roomInst
+		
+		return roomInst
+
+func generate_next(location):
+	create_room(location.x+1, location.y)
+	create_room(location.x-1, location.y)
+	create_room(location.x, location.y+1)
+	create_room(location.x, location.y-1)
