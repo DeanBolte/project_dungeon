e0fd00acc99cef6aa4cd06a59b30769fd622899a
Dean Bolte <dean.bolte7@gmail.com>
2022-12-05 19:58:41 +1100

    removed duplicate code and unnecessary next lines

--- Enemy/SingleShotEnemy.gd
+++ Enemy/SingleShotEnemy.gd
@@ -21,25 +21,9 @@ func _ready():
 	
 	state = pick_rand_state([IDLE, WANDER])
 
-func _physics_process(delta):
-	# check health
-	if health <= 0:
-		queue_free()
-	
-	match state:
-		IDLE:
-			idle(delta)
-		WANDER:
-			wander(delta)
-		CHASE:
-			chase_player(delta)
-	
-	velocity = move_and_slide(velocity)
-
 func _on_HurtBox_body_entered(body):
 	decrement_health(body.damage)
 	recoil(-body.direction, 400)
-
 
 func _on_PlayerDetectionCycle_timeout():
 	if playerDetectionZone.can_see_player():
