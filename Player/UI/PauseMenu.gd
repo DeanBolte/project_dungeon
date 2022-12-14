d3c6e8e5d70a3426d91513c2a02ab32308697644
Dean Bolte <dean.bolte7@gmail.com>
2022-12-14 22:09:39 +1100

    unlocked cursor in pause menu

--- Player/UI/PauseMenu.gd
+++ Player/UI/PauseMenu.gd
@@ -10,6 +10,11 @@ func set_is_paused(value):
 	is_paused = value
 	get_tree().paused = is_paused
 	visible = is_paused
+	
+	if is_paused:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
 
 func _on_ResumeButton_pressed():
 	self.is_paused = false
