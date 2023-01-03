91766fdf7e4c71e16c53def0e8ace0d675f55f43
Dean Bolte <dean.bolte7@gmail.com>
2023-01-03 19:46:03 +1100

    sliders now modify the correct audio bus

--- MainMenu/VolumeMixer.gd
+++ MainMenu/VolumeMixer.gd
@@ -1,11 +1,18 @@
 extends VBoxContainer
 
-var master_bus = AudioServer.get_bus_index("Music")
+var master_bus = AudioServer.get_bus_index("Master")
+var music_bus = AudioServer.get_bus_index("Music")
+var sfx_bus = AudioServer.get_bus_index("SFX")
 
-func _on_HSlider_value_changed(value):
-	AudioServer.set_bus_volume_db(master_bus, value)
-	
+func change_bus_volume(bus_index: int, value: float):
+	AudioServer.set_bus_volume_db(bus_index, value)
 	if value == -30:
-		AudioServer.set_bus_mute(master_bus, true)
+		AudioServer.set_bus_mute(bus_index, true)
 	else:
-		AudioServer.set_bus_mute(master_bus, false)
+		AudioServer.set_bus_mute(bus_index, false)
+
+func _on_SFXSlider_value_changed(value):
+	change_bus_volume(sfx_bus, value)
+
+func _on_MusicSlider_value_changed(value):
+	change_bus_volume(music_bus, value)
