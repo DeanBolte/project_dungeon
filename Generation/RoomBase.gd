566e2d4c9972cf5b37273d4a91b8ee43aa3245ae
Dean Bolte <dean.bolte7@gmail.com>
2023-06-09 19:42:45 +1000

    added crate based obstacles

--- Generation/RoomBase.gd
+++ Generation/RoomBase.gd
@@ -3,9 +3,13 @@ extends Node2D
 onready var EnemiesActive: Node = $EnemiesActive
 onready var FlowerTileMap: TileMap = $FlowerTileMap
 
+var BoxPileScene = preload("res://Generation/Obstacles/BoxPile.tscn")
+
 signal first_entered(location)
 
 export (Vector2) var MAP_LOCATION
+export var ROOM_CENTRE = Vector2(320, 320)
+export var OBSTACLE_SPREAD = 200
 export var FLOWER_TILE_COUNT = 40
 export var TILES_TO_CENTRE = 10
 export var TILE_SPREAD = 10
@@ -14,6 +18,13 @@ var visited = false
 
 func generate_objects():
 	generate_flowers()
+	generate_obstacles()
+
+func generate_obstacles():
+	for i in range(randi() % 4):
+		var boxpileinst = BoxPileScene.instance()
+		add_child(boxpileinst)
+		boxpileinst.global_position = global_position + ROOM_CENTRE + Vector2(rand_range(-OBSTACLE_SPREAD, OBSTACLE_SPREAD), rand_range(-OBSTACLE_SPREAD, OBSTACLE_SPREAD))
 
 func generate_flowers():
 	for i in range(FLOWER_TILE_COUNT):
