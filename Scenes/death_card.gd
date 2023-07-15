f2b299d005a24b5fb96f159c635e94ae7acd8fb6
Dean Bolte <dean.bolte7@gmail.com>
2023-07-15 19:04:41 +1000

    added subtext in deathcard to action delay

--- Scenes/death_card.gd
+++ Scenes/death_card.gd
@@ -1,14 +1,21 @@
 extends Control
 
+onready var SubtextLabel: Label = $VBoxContainer/Subtext
+
 var MainMenuScene = "res://MainMenu/Menu.tscn"
 
 export var ACTION_DELAY_SECONDS = 1
 
 var actionDelay = ACTION_DELAY_SECONDS
 
+func _ready() -> void:
+	SubtextLabel.visible = false
+
 func _process(delta: float) -> void:
 	if actionDelay > 0:
 		actionDelay -= delta
+	else:
+		SubtextLabel.visible = true
 
 func _input(event: InputEvent) -> void:
 	if actionDelay <= 0:
