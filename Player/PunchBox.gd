3c8bea9a9483e1e14bfeec37974b6b99f33be3af
Dean Bolte <dean.bolte7@gmail.com>
2023-09-11 18:03:22 +1000

    added some better code for punching

--- Player/PunchBox.gd
+++ Player/PunchBox.gd
@@ -3,5 +3,5 @@ extends Area2D
 const PUNCH_VELOCITY_MULTIPLIER = 2
 
 func _on_PunchBox_area_entered(area):
-	# this is pretty shoddy
-	area.get_parent().take_hit(2, $"..".aimingNormalVector * PUNCH_VELOCITY_MULTIPLIER)
+	if area.get_parent().has_method('take_hit'):
+		area.get_parent().take_hit(2, get_parent().aimingNormalVector * PUNCH_VELOCITY_MULTIPLIER)
