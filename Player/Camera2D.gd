e7033c8e233b4a21fbb984476ec242069d886228
Dean Bolte <dean.bolte7@gmail.com>
2023-09-16 12:03:11 +1000

    added some bleed effects

--- Player/Camera2D.gd
+++ Player/Camera2D.gd
@@ -1,10 +1,9 @@
 extends Camera2D
 
-@export var CAMERA_OFFSET_SCALE = 20
+@export var CAMERA_OFFSET_SCALE = 4
 
 @onready var Player = $".."
 
 func _physics_process(_delta: float) -> void:
 	var centreOffset: Vector2 = (get_global_mouse_position() - Player.global_position)
-	var cameraOffset: Vector2 = centreOffset.normalized() * sqrt(centreOffset.length() * CAMERA_OFFSET_SCALE)
-	global_position = Player.global_position + cameraOffset
+	global_position = Player.global_position + centreOffset / CAMERA_OFFSET_SCALE
